4c854c8659bae769f1b51cf660d473f9a58d4eb4
SoFranci <trevisanfrancesco022@gmail.com>
2023-11-28 20:56:18 +0100

    initial commit

---
+++ platforms/ios/www/game/guess-the-number/script.js
@@ -0,0 +1,42 @@
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+function checkGuess() {
+    const guessInput = document.getElementById('guessInput');
+    const message = document.getElementById('message');
+
+    if (guessInput.value === '') {
+        message.innerText = 'Perfavore metti un numero';
+        return;
+    }
+
+    const playerGuess = parseInt(guessInput.value);
+
+    if (isNaN(playerGuess) || playerGuess < 1 || playerGuess > 100) {
+        message.innerText = 'Perfavore metti un numero valido tra 1 e 100';
+        return;
+    }
+
+    attempts++;
+
+    if (playerGuess === secretNumber) {
+        message.innerText = `Congratulazioni hai indovinato il numero in ${attempts} tentativi.`;
+        resetGame();
+    } else if (playerGuess < secretNumber) {
+        message.innerText = 'Troppo basso. Riprova';
+    } else {
+        message.innerText = 'Troppo alto. Riprova!';
+    }
+
+    guessInput.value = '';
+}
+
+function resetGame() {
+    attempts = 0;
+    secretNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const message = document.getElementById('message');
+    message.innerText = 'Prova ad indovinare un numero tra 1 e 100:';
+});
